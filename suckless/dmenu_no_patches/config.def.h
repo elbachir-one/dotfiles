6c39a8473c7d9f4fb850a6f1c245ea8e8201731d
EL BACHIR <bachiralfa@gmail.com>
2022-10-29 06:30:37 +0100

    dmenu no patches just config change

--- suckless/dmenu_no_patches/config.def.h
+++ suckless/dmenu_no_patches/config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:style=Bold:size=14"
+	"terminus:style=Bold:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
