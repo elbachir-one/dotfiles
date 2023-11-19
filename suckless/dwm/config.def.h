b96249bef7023a23ed877c1483e55c2aea8ae9cf
alpha b 911 <bachiralfa@gmail.com>
2023-11-19 18:40:57 +0100

    Clean Up

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx     = 1;        /* border pixel of windows */
+static const unsigned int borderpx     = 0;        /* border pixel of windows */
 static const int startwithgaps[]       = { 1 };	/* 1 means gaps are used by default, this can be customized for each tag */
 static const unsigned int gappx[]      = { 2 };   /* default gap between windows in pixels, this can be customized for each tag */
 static const unsigned int snap         = 8;       /* snap pixel */
