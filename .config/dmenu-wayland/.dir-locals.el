2b3d5192cc6b72107c641803da692fca8ce01844
alpha b 911 <bachiralfa@gmail.com>
2023-12-30 00:49:57 +0100

    Clean up

--- .config/dmenu-wayland/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-
-((nil . ((tab-width . 4)
-         (c-basic-offset . 4)
-         (sentence-end-double-space . t)
-         (indent-tabs-mode . t)
-         (fill-column . 80)))
- (c-mode . ((c-file-style . "GNU")
-            (c-noise-macro-names . ("UNINIT" "CALLBACK" "ALIGN_STACK")))))
