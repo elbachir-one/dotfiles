c7b32bb5acfa998927981d6521e759f3be37e40b
alpha b 911 <bachiralfa@gmail.com>
2024-01-05 03:38:36 +0100

    Fixed an issue with slstatus

--- suckless/slstatus/config.def.h
+++ suckless/slstatus/config.def.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function 	 	 			format          			argument */
-	{ cpu_perc, 			"[﬙ %.5s%%]", 						NULL},
+	{ cpu_perc, 			" [﬙ %.5s%%]", 						NULL},
 	{ ram_perc, 			"[ %.5s%%]", 						NULL},
 	{ disk_perc,  			"[ %.5s%%]",						"/"},
 };
