fe12ddeb282449a56a76307f0ddb884d9a06856a
elbachir-one <bachiralfa@gmail.com>
2025-05-08 18:00:38 +0100

    Cleaned up the install.sh script

--- install.sh
+++ install.sh
@@ -1,73 +1,106 @@
 #!/bin/bash
-sudo sed -i 's/%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers.d/wheel
+set -e  # Exit on any error
+set -o pipefail
+
+USERNAME=$(logname)
+
+# Allow wheel group passwordless sudo
+sudo sed -i 's/^%wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers.d/wheel
+
+# System update
 sudo xbps-install -Suy
-sudo xbps-install -Sy xorg base-devel libX11-devel libXft-devel libXinerama-devel\
-chromium ffmpeg ntfs-3g ugrep noto-fonts-emoji noto-fonts-cjk feh lsd\
-webkit2gtk-devel gcr-devel gstreamer1-devel lxappearance neovim clipmenu mpv mpd\
-alsa-utils ncmpcpp cava newsboat zathura mupdf ranger ueberzug qutebrowser sakura\
-w3m nodejs gimp bash-completion yt-dlp aria2 neofetch flameshot cmake ninja meson\
-curl ImageMagick NetworkManager arandr bat breeze clang cmatrix lolcat-c figlet\
-colordiff timeshift flac fzf git gstreamer-vaapi harfbuzz-devel htop imlib2-devel\
-jq kdenlive libev-devel libjpeg-turbo-devel libmpc-devel linux-headers man-db mpc\
-papirus-folders papirus-icon-theme pcre-devel pkgconf-devel python3-adblock\
-python3-pip rsync simple-mtpfs terminus-font v4l2loopback void-docs-browse\
-xdg-desktop-portal-wlr xdotool zathura-pdf-mupdf tmux xcb-util-renderutil-devel\
-xcb-util-image-devel pkgconf uthash pcre-devel libconfig-devel figlet-fonts dunst\
-noto-fonts-ttf pass wkhtmltopdf audacity readline-devel readline file-devel\
-plata-theme img2pdf pcmanfm cups cups-pdf libinput-gestures
 
-# This for doing testing on different stuff
-# sudo xbps-install -Sy river sway Waybar sandbar foot pamixer fcft-devel droidcam asciinema polybar bspwm sxhkd grim slop slurp tor tint2 openbox xfce4 obmenu-generator obconf hplip-gui clipman nerd-fonts wireshark-qt wireshark nim passmenu cvs unbound poppler-utils lf i3-gaps i3lock vim mupdf linux-lts scrot qemu virt-manager libvirt vte3 vde2 bridge-utils time screenkey clang-analyzer cmatrix emacs ffplay gdb go zig gvim hugo inkscape instaloader intel-media-driver linux-lts-headers pandoc pdftag rnnoise slop stow tcc texlive texlive-core wofi wbg Mousai NoiseTorch SDL2_gfx-devel SDL2_image-devel SDL2_ttf-devel SDL-devel aircrack-ng WoeUSB alsa-rnnoise alsa-plugins-ffmpeg android-tools alsa-plugins-jack barrier-gui bluez-alsa catimg clang-tools-extra cloc cool-retro-term cwm docker docbook dotool droidcam droidcam-obs-plugin farbfeld fff ffmpegthumbs ffscreencast figlet-fonts flashrom font-iosevka github-cli glade3 giflib-tools glances groff-doc hyperfine joe libmagick-devel linux-lts most mpv-mpris mutt nemo newsraft nginx openvpn pass-import pass-otp patchutils progress python3-adblock qt5ct rnnoise rofi-emoji rtkit spice-vdagent termrec tigervnc tty-clock typespeed unclutter virt-viewer waydroid weechat wf-recorder wine wlr-randr wlroots0.17-devel wlroots0.17 wayvnc xdg-desktop-portal-wlr xorg-server-xephyr xtools zig ack valgrind time linuxwave mako resynthesizer
+# Install packages
+sudo xbps-install -Sy \
+	xorg base-devel libX11-devel libXft-devel libXinerama-devel chromium ffmpeg ntfs-3g \
+	ugrep noto-fonts-emoji noto-fonts-cjk feh lsd webkit2gtk-devel gcr-devel gstreamer1-devel \
+	lxappearance neovim clipmenu mpv mpd alsa-utils ncmpcpp cava newsboat zathura mupdf \
+	ranger ueberzug sakura nodejs bash-completion yt-dlp aria2 neofetch stow \
+	flameshot cmake ninja meson curl ImageMagick NetworkManager arandr bat breeze clang \
+	cmatrix lolcat-c figlet colordiff timeshift flac fzf git gstreamer-vaapi harfbuzz-devel \
+	htop imlib2-devel jq libev-devel libjpeg-turbo-devel libmpc-devel linux-headers \
+	man-db mpc papirus-folders papirus-icon-theme pcre-devel pkgconf-devel python3-adblock \
+	python3-pip rsync simple-mtpfs terminus-font v4l2loopback \
+	xdg-desktop-portal-wlr xdotool zathura-pdf-mupdf tmux xcb-util-renderutil-devel \
+	xcb-util-image-devel pkgconf uthash pcre-devel libconfig-devel figlet-fonts dunst \
+	pass wkhtmltopdf audacity readline-devel readline file-devel plata-theme \
+	img2pdf cups cups-pdf libinput-gestures
 
+# Compile and install picom
 cd /tmp/
 git clone --depth=1 https://github.com/pijulius/picom.git
-cd picom/
+cd picom
 git submodule update --init --recursive
 meson --buildtype=release . build
 sudo ninja -C build install
-cd
-mkdir Videos Images Downloads Documents Music 
+
+# Create user directories
+cd ~
+mkdir -p Videos Images Downloads Documents Music
+
+# Clone and copy dotfiles
 git clone --depth=1 https://github.com/elbachir-one/dotfiles
-cd ~/dotfiles/
-cp -r .config/ .fonts/ .icons/ .local/ .themes/ .bashrc .xinitrc .tmux.conf .bash_profile ~/
+cd dotfiles
+cp -r .config .fonts .icons .local .themes .bashrc .xinitrc .tmux.conf .bash_profile ~/
 sudo cp -r xorg.conf.d/ /etc/X11/
-cd suckless/dwm/ && make && sudo make clean install
-cd ../dmenu/ && make && sudo make clean install
-cd ../st/ && make && sudo make clean install
-cd ../slstatus/ && make && sudo make clean install
-cd ../slock/ && make && sudo make clean install
-cd ../farbfeld/ && make && sudo make clean install
-cd ../sent/ && make && sudo make clean install
-cd ../surf/ && make && sudo make clean install
-cd
+
+# Build suckless programs
+for prog in dwm dmenu st slstatus slock farbfeld sent surf; do
+	cd ~/dotfiles/suckless/$prog
+	make
+	sudo make install
+done
+
+# Clone wallpapers
+cd ~
 git clone --depth=1 https://github.com/elbachir-one/wall
+
+# Install grc
 cd /tmp/
 git clone https://github.com/garabik/grc.git
-cd grc/
+cd grc
 sudo ./install.sh
-cd
 sudo cp /etc/profile.d/grc.sh /etc/
-sudo xbps-remove -Oy && sudo xbps-remove -oy
-#sudo xbps-reconfigure -f linux-lts
+
+# Clean up orphan packages
+sudo xbps-remove -Oy
+sudo xbps-remove -oy
+
+# Reconfigure all packages
 sudo xbps-reconfigure -fa
+
+# Update font cache
 fc-cache -fv
+
+# Switch to NetworkManager
 sudo sv down dhcpcd
-sudo ln -s /etc/sv/NetworkManager /var/service/
-sudo ln -s /etc/sv/dbus /var/service/
-#sudo ln -s /etc/sv/libvirtd /var/service/
-#sudo ln -s /etc/sv/virtlogd /var/service/
-#sudo ln -s /etc/sv/seatd /var/service/
-#sudo usermod -a -G _seatd sh # Change the username (sh) to your username.
-#sudo usermod -a -G libvirt sh # Change the username (sh) to your username than run the script.
-#modprobe kvm-intel # Change this if you are using something else that intel cpu "kvm-amd"
-#sudo usermod -aG kvm sh # Change the username (sh) to your username than run the script.
-sudo rm /var/service/agetty-tty3
-sudo rm /var/service/agetty-tty4
-sudo rm /var/service/agetty-tty5
-sudo rm /var/service/agetty-tty6
-sudo rm /var/service/wpa_supplicant
-sudo rm /var/service/dhcpcd
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo sed -i 's/GETTY_ARGS="--noclear"/GETTY_ARGS="--noclear --autologin sh"/' /etc/runit/runsvdir/current/agetty-tty1/conf # Change the username (sh) to your username than run the script.
+sudo ln -sf /etc/sv/NetworkManager /var/service/
+sudo ln -sf /etc/sv/dbus /var/service/
+
+# Optional service links (uncomment if needed)
+# sudo ln -s /etc/sv/libvirtd /var/service/
+# sudo ln -s /etc/sv/virtlogd /var/service/
+# sudo ln -s /etc/sv/seatd /var/service/
+# sudo usermod -aG _seatd "$USERNAME"
+# sudo usermod -aG libvirt "$USERNAME"
+# sudo modprobe kvm-intel  # Use 'kvm-amd' for AMD CPUs
+# sudo usermod -aG kvm "$USERNAME"
+
+# Remove unwanted ttys and services
+for tty in tty3 tty4 tty5 tty6; do
+	sudo rm -f /var/service/agetty-$tty
+done
+
+sudo rm -f /var/service/wpa_supplicant
+sudo rm -f /var/service/dhcpcd
+
+# GRUB and auto-login config
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+sudo sed -i "s/GETTY_ARGS=\"--noclear\"/GETTY_ARGS=\"--noclear --autologin $USERNAME\"/" \
+	/etc/runit/runsvdir/current/agetty-tty1/conf
+
+# Update GRUB config
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Reboot system
 sudo reboot
