2f8e4a4d33d1a507bd9d37b7d223999567ae1c17
elbachir-one <bachiralfa@gmail.com>
2026-07-22 20:18:36 +0100

    Improved

--- suckless/dwm/exitdwm.c
+++ suckless/dwm/exitdwm.c
@@ -16,23 +16,26 @@ void exitdwm ()
 	defined S_EXIT_ICON			|| \
 	defined S_REBOOT_ICON		|| \
 	defined S_SHUTDOWN_ICON		|| \
+	defined S_SLEEP_ICON		|| \
 	defined S_FORMAT			|| \
 	defined S_FORMAT_CLEAR
 # error (conflicting macro names)
 # endif
 
-# define S_LOCK "Lock"
-# define S_RESTART_DWM "restart Dwm"
-# define S_OFFSCREEN "Off-screen"
-# define S_EXIT "Exit"
-# define S_REBOOT "Reboot"
-# define S_SHUTDOWN "Shutdown"
-# define S_LOCK_ICON "\uf023"			// <= FontAwesome icons
+# define S_LOCK " Lock"
+# define S_RESTART_DWM " Restart Dwm"
+# define S_OFFSCREEN " Turn Off Screen"
+# define S_EXIT " Exit"
+# define S_REBOOT " Reboot"
+# define S_SHUTDOWN " Shutdown"
+# define S_SLEEP " Sleep"
+# define S_LOCK_ICON "\uf023"
 # define S_RESTART_DWM_ICON "\uf01e"
 # define S_OFFSCREEN_ICON "\uf108"
 # define S_EXIT_ICON "\uf2f5"
 # define S_REBOOT_ICON "\uf021"
 # define S_SHUTDOWN_ICON "\uf011"
+#define S_SLEEP_ICON "\uf186"
 
 # define S_FORMAT(ACTION) S_##ACTION##_ICON " " S_##ACTION
 # define S_FORMAT_CLEAR "sed 's/^..//'"
@@ -42,10 +45,11 @@ void exitdwm ()
 			S_FORMAT (LOCK) "\n"
 			S_FORMAT (RESTART_DWM) "\n"
 			S_FORMAT (OFFSCREEN) "\n"
+			S_FORMAT (SLEEP) "\n"
 			S_FORMAT (EXIT) "\n"
 			S_FORMAT (REBOOT) "\n"
 			S_FORMAT (SHUTDOWN)
-			"\" | dmenu -p exit: | " S_FORMAT_CLEAR
+			"\" | dmenu -p Control  | " S_FORMAT_CLEAR
 		,
 		"r"
 	);
@@ -64,6 +68,7 @@ void exitdwm ()
 	else if (strcmp (exit_action, S_RESTART_DWM) == 0) quit (& (const Arg) {1});
 	else if (strcmp (exit_action, S_OFFSCREEN) == 0) system ("sleep .5; xset dpms force off");
 	else if (strcmp (exit_action, S_EXIT) == 0) quit (& (const Arg) {0});
+	else if (strcmp (exit_action, S_SLEEP) == 0) system ("sudo zzz");
 	else if (strcmp (exit_action, S_REBOOT) == 0) system ("sudo reboot");
 	else if (strcmp (exit_action, S_SHUTDOWN) == 0) system ("sudo poweroff -i");
 
@@ -74,12 +79,14 @@ close_streams:
 # undef S_RESTART_DWM
 # undef S_OFFSCREEN
 # undef S_EXIT
+# undef S_SLEEP
 # undef S_REBOOT
 # undef S_SHUTDOWN
 # undef S_LOCK_ICON
 # undef S_RESTART_DWM_ICON
 # undef S_OFFSCREEN_ICON
 # undef S_EXIT_ICON
+# undef S_SLEEP_ICON
 # undef S_REBOOT_ICON
 # undef S_SHUTDOWN_ICON
 # undef S_FORMAT
